84755615568d3e73a3569d22976f6d0c420623c4
Ando Yasushi <andyjpn@gmail.com>
2014-09-12 14:18:47 +0900

    とりあえず

--- js/ui.js
+++ js/ui.js
@@ -27,3 +27,35 @@ document.getElementById('shake-button').addEventListener('click', function(event
   oppai.shake();
 });
 
+
+
+/*とりあえず*/
+document.getElementById('sidebar-button').addEventListener('touchend', function(event) {
+  if (event.target.classList.contains('close')) {
+    event.target.classList.remove('close');
+    event.target.classList.add('open');
+    document.getElementById('sidebar').className = 'open';
+  }
+  else {
+    event.target.classList.remove('open');
+    event.target.classList.add('close');
+    document.getElementById('sidebar').className = 'close';
+  }
+});
+document.getElementById('plane-button').addEventListener('touchend', function(event) {
+  oppai.threeMesh.material.wireframe = false;
+  if (oppai2) oppai2.threeMesh.material.wireframe = false;
+  hand.threeFinger.material.wireframe = false;
+});
+document.getElementById('mesh-button').addEventListener('touchend', function(event) {
+  oppai.threeMesh.material.wireframe = true;
+  if (oppai2) oppai2.threeMesh.material.wireframe = true;
+  hand.threeFinger.material.wireframe = true;
+});
+document.getElementById('throw-button').addEventListener('touchend', function(event) {
+  hand.touch();
+});
+document.getElementById('shake-button').addEventListener('touchend', function(event) {
+  oppai.shake();
+});
+
