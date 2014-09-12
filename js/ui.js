575338de60cd996db7d35c493069e58b19874428
Ando Yasushi <andyjpn@gmail.com>
2014-09-13 08:22:26 +0900

    change ui

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,6 @@
-document.getElementById('sidebar-button').addEventListener('click', function(event) {
+function clickEvent() { return Oppai.isSmartphone ? 'touchend' : 'click'; }
+
+document.getElementById('sidebar-button').addEventListener(clickEvent(), function(event) {
   if (event.target.classList.contains('close')) {
     event.target.classList.remove('close');
     event.target.classList.add('open');
@@ -10,52 +12,25 @@ document.getElementById('sidebar-button').addEventListener('click', function(eve
     document.getElementById('sidebar').className = 'close';
   }
 });
-document.getElementById('plane-button').addEventListener('click', function(event) {
+document.getElementById('plane-button').addEventListener(clickEvent(), function(event) {
   oppai.threeMesh.material.wireframe = false;
   if (oppai2) oppai2.threeMesh.material.wireframe = false;
   hand.threeFinger.material.wireframe = false;
 });
-document.getElementById('mesh-button').addEventListener('click', function(event) {
+document.getElementById('mesh-button').addEventListener(clickEvent(), function(event) {
   oppai.threeMesh.material.wireframe = true;
   if (oppai2) oppai2.threeMesh.material.wireframe = true;
   hand.threeFinger.material.wireframe = true;
 });
-document.getElementById('throw-button').addEventListener('click', function(event) {
+document.getElementById('throw-button').addEventListener(clickEvent(), function(event) {
   hand.touch();
 });
-document.getElementById('shake-button').addEventListener('click', function(event) {
+document.getElementById('shake-button').addEventListener(clickEvent(), function(event) {
   oppai.shake();
 });
-
-
-
-/*とりあえず*/
-document.getElementById('sidebar-button').addEventListener('touchend', function(event) {
-  if (event.target.classList.contains('close')) {
-    event.target.classList.remove('close');
-    event.target.classList.add('open');
-    document.getElementById('sidebar').className = 'open';
-  }
-  else {
-    event.target.classList.remove('open');
-    event.target.classList.add('close');
-    document.getElementById('sidebar').className = 'close';
-  }
+document.getElementById('turn-right-button').addEventListener(clickEvent(), function(event) {
+  controls.angle -= Math.PI * 0.01;
 });
-document.getElementById('plane-button').addEventListener('touchend', function(event) {
-  oppai.threeMesh.material.wireframe = false;
-  if (oppai2) oppai2.threeMesh.material.wireframe = false;
-  hand.threeFinger.material.wireframe = false;
+document.getElementById('turn-left-button').addEventListener(clickEvent(), function(event) {
+  controls.angle += Math.PI * 0.01;
 });
-document.getElementById('mesh-button').addEventListener('touchend', function(event) {
-  oppai.threeMesh.material.wireframe = true;
-  if (oppai2) oppai2.threeMesh.material.wireframe = true;
-  hand.threeFinger.material.wireframe = true;
-});
-document.getElementById('throw-button').addEventListener('touchend', function(event) {
-  hand.touch();
-});
-document.getElementById('shake-button').addEventListener('touchend', function(event) {
-  oppai.shake();
-});
-
